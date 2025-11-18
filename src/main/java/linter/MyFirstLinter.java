28e7ecb5b8646a8e3b5768b74d82ba84edd79e87
AnantPoddar1004 <anantpoddar2026@u.northwestern.edu>
2025-11-18 17:23:38 -0600

    Redundant Interface Check Added

--- src/main/java/linter/MyFirstLinter.java
+++ src/main/java/linter/MyFirstLinter.java
@@ -1,13 +1,14 @@
 package linter;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.FieldNode;
-import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.tree.*;
 
 // FIXME: this code has TERRIBLE DESIGN all around
 public class MyFirstLinter {
@@ -42,7 +43,7 @@ public class MyFirstLinter {
             reader.accept(classNode, ClassReader.EXPAND_FRAMES);
 
             // Now we can navigate the classNode and look for things we are interested in.
-//            printClass(classNode);
+            printClass(classNode);
 //
 //            printFields(classNode);
 //
@@ -52,98 +53,101 @@ public class MyFirstLinter {
 
             hasPublicMutableFields(classNode);
 
-//            hasRedundantInterface(classNode);
+            redundantInterfaces(classNode);
+
+            System.out.println();
+            System.out.println();
         }
     }
 
-//    private static void printClass(ClassNode classNode) {
-//        System.out.println("Class's Internal JVM name: " + classNode.name);
-//        System.out.println("User-friendly name: "
-//                + Type.getObjectType(classNode.name).getClassName());
-//        System.out.println("public? "
-//                + ((classNode.access & Opcodes.ACC_PUBLIC) != 0));
-//        System.out.println("Extends: " + classNode.superName);
-//        System.out.println("Implements: " + classNode.interfaces);
-//        // TODO: how do I write a lint check to tell if this class has a bad name?
-//    }
-//
-//    private static void printFields(ClassNode classNode) {
-//        // Print all fields (note the cast; ASM doesn't store generic data with its Lists)
-//        List<FieldNode> fields = (List<FieldNode>) classNode.fields;
-//        for (FieldNode field : fields) {
-//            System.out.println("	Field: " + field.name);
-//            System.out.println("	Internal JVM type: " + field.desc);
-//            System.out.println("	User-friendly type: "
-//                    + Type.getObjectType(field.desc).getClassName());
-//            // Query the access modifiers with the ACC_* constants.
-//
-//            System.out.println("	public? "
-//                    + ((field.access & Opcodes.ACC_PUBLIC) != 0));
-//            // TODO: how do you tell if something has package-private access? (ie no access modifiers?)
-//
-//            // TODO: how do I write a lint check to tell if this field has a bad name?
-//
-//            System.out.println();
-//        }
-//    }
-//
-//    private static void printMethods(ClassNode classNode) {
-//        List<MethodNode> methods = (List<MethodNode>) classNode.methods;
-//        for (MethodNode method : methods) {
-//            System.out.println("	Method: " + method.name);
-//            System.out
-//                    .println("	Internal JVM method signature: " + method.desc);
-//
-//            System.out.println("	Return type: "
-//                    + Type.getReturnType(method.desc).getClassName());
-//
-//            System.out.println("	Args: ");
-//            for (Type argType : Type.getArgumentTypes(method.desc)) {
-//                System.out.println("		" + argType.getClassName());
-//                // FIXME: what is the argument's *variable* name?
-//            }
-//
-//            System.out.println("	public? "
-//                    + ((method.access & Opcodes.ACC_PUBLIC) != 0));
-//            System.out.println("	static? "
-//                    + ((method.access & Opcodes.ACC_STATIC) != 0));
-//            // How do you tell if something has default access? (ie no access modifiers?)
-//
-//            System.out.println();
-//
-//            // Print the method's instructions
-//            printInstructions(method);
-//        }
-//    }
-//
-//    private static void printInstructions(MethodNode methodNode) {
-//        InsnList instructions = methodNode.instructions;
-//        for (int i = 0; i < instructions.size(); i++) {
-//
-//            // We don't know immediately what kind of instruction we have.
-//            AbstractInsnNode insn = instructions.get(i);
-//
-//            // FIXME: Is instanceof the best way to deal with the instruction's type?
-//            if (insn instanceof MethodInsnNode) {
-//                // A method call of some sort; what other useful fields does this object have?
-//                MethodInsnNode methodCall = (MethodInsnNode) insn;
-//                System.out.println("		Call method: " + methodCall.owner + " "
-//                        + methodCall.name);
-//            } else if (insn instanceof VarInsnNode) {
-//                // Some some kind of variable *LOAD or *STORE operation.
-//                VarInsnNode varInsn = (VarInsnNode) insn;
-//                int opCode = varInsn.getOpcode();
-//                // See VarInsnNode.setOpcode for the list of possible values of
-//                // opCode. These are from a variable-related subset of Java
-//                // opcodes.
-//            }
-//            // There are others...
-//            // This list of direct known subclasses may be useful:
-//            // http://asm.ow2.org/asm50/javadoc/user/org/objectweb/asm/tree/AbstractInsnNode.html
-//
-//            // TODO: how do I write a lint check to tell if this method has a bad name?
-//        }
-//    }
+    private static void printClass(ClassNode classNode) {
+        System.out.println("Class's Internal JVM name: " + classNode.name);
+        System.out.println("User-friendly name: "
+                + Type.getObjectType(classNode.name).getClassName());
+        System.out.println("public? "
+                + ((classNode.access & Opcodes.ACC_PUBLIC) != 0));
+        System.out.println("Extends: " + classNode.superName);
+        System.out.println("Implements: " + classNode.interfaces);
+        // TODO: how do I write a lint check to tell if this class has a bad name?
+    }
+
+    private static void printFields(ClassNode classNode) {
+        // Print all fields (note the cast; ASM doesn't store generic data with its Lists)
+        List<FieldNode> fields = (List<FieldNode>) classNode.fields;
+        for (FieldNode field : fields) {
+            System.out.println("	Field: " + field.name);
+            System.out.println("	Internal JVM type: " + field.desc);
+            System.out.println("	User-friendly type: "
+                    + Type.getObjectType(field.desc).getClassName());
+            // Query the access modifiers with the ACC_* constants.
+
+            System.out.println("	public? "
+                    + ((field.access & Opcodes.ACC_PUBLIC) != 0));
+            // TODO: how do you tell if something has package-private access? (ie no access modifiers?)
+
+            // TODO: how do I write a lint check to tell if this field has a bad name?
+
+            System.out.println();
+        }
+    }
+
+    private static void printMethods(ClassNode classNode) {
+        List<MethodNode> methods = (List<MethodNode>) classNode.methods;
+        for (MethodNode method : methods) {
+            System.out.println("	Method: " + method.name);
+            System.out
+                    .println("	Internal JVM method signature: " + method.desc);
+
+            System.out.println("	Return type: "
+                    + Type.getReturnType(method.desc).getClassName());
+
+            System.out.println("	Args: ");
+            for (Type argType : Type.getArgumentTypes(method.desc)) {
+                System.out.println("		" + argType.getClassName());
+                // FIXME: what is the argument's *variable* name?
+            }
+
+            System.out.println("	public? "
+                    + ((method.access & Opcodes.ACC_PUBLIC) != 0));
+            System.out.println("	static? "
+                    + ((method.access & Opcodes.ACC_STATIC) != 0));
+            // How do you tell if something has default access? (ie no access modifiers?)
+
+            System.out.println();
+
+            // Print the method's instructions
+            printInstructions(method);
+        }
+    }
+
+    private static void printInstructions(MethodNode methodNode) {
+        InsnList instructions = methodNode.instructions;
+        for (int i = 0; i < instructions.size(); i++) {
+
+            // We don't know immediately what kind of instruction we have.
+            AbstractInsnNode insn = instructions.get(i);
+
+            // FIXME: Is instanceof the best way to deal with the instruction's type?
+            if (insn instanceof MethodInsnNode) {
+                // A method call of some sort; what other useful fields does this object have?
+                MethodInsnNode methodCall = (MethodInsnNode) insn;
+                System.out.println("		Call method: " + methodCall.owner + " "
+                        + methodCall.name);
+            } else if (insn instanceof VarInsnNode) {
+                // Some some kind of variable *LOAD or *STORE operation.
+                VarInsnNode varInsn = (VarInsnNode) insn;
+                int opCode = varInsn.getOpcode();
+                // See VarInsnNode.setOpcode for the list of possible values of
+                // opCode. These are from a variable-related subset of Java
+                // opcodes.
+            }
+            // There are others...
+            // This list of direct known subclasses may be useful:
+            // http://asm.ow2.org/asm50/javadoc/user/org/objectweb/asm/tree/AbstractInsnNode.html
+
+            // TODO: how do I write a lint check to tell if this method has a bad name?
+        }
+    }
 
     private static void hasEqualsAndHashcode(ClassNode classNode){
         List<MethodNode> methods = (List<MethodNode>) classNode.methods;
@@ -177,5 +181,41 @@ public class MyFirstLinter {
         }
 
         System.out.println("Check for Public Mutable Fields Completed");
+    }
+
+    private static void redundantInterfaces(ClassNode classNode) throws IOException
+    {
+        String superClassName = Type.getObjectType(classNode.superName).getClassName();
+        List<String> interfaces = (List<String>) classNode.interfaces;
+
+        System.out.println("superClassName = " + superClassName);
+        System.out.println("interfaces = " + interfaces);
+
+        Set<String> parentInterfacesSet = parentInterfaces(superClassName);
+
+        for (String interfaceName : interfaces) {
+            if (parentInterfacesSet.contains(interfaceName)) {
+                System.out.println("Redundant Interface: " + interfaceName);
+            }
+        }
+
+    }
+
+    private static Set<String> parentInterfaces(String superClassName) throws IOException
+    {
+        Set<String> interfacesSet = new HashSet<>();
+
+        while ((superClassName != null) && !superClassName.equals("java.lang.Object")) {
+            ClassReader reader = new ClassReader(superClassName);
+            ClassNode superClassNode = new ClassNode();
+            reader.accept(superClassNode, ClassReader.EXPAND_FRAMES);
+
+            List<String> interfaces = (List<String>) superClassNode.interfaces;
+            interfacesSet.addAll(interfaces);
+
+            superClassName = Type.getObjectType(superClassNode.superName).getClassName();
+        }
+        return interfacesSet;
+
     }
 }
